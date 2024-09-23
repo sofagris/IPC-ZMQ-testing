7803c83b9351ad095d2f30609f9efa811e6356cf
Sofagris <roy@stua.net>
2024-09-23 21:06:10 +0200

    Translations..

--- zmq-sender.py
+++ zmq-sender.py
@@ -5,8 +5,8 @@ context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://127.0.0.1:5555")
 
-message = "Hei fra kommandolinjeapplikasjonen"
+message = "Hello from the client"
 socket.send_string(message)
 
 reply = socket.recv_string()
-print(f"Server svarte: {reply}")
+print(f"Server replied: {reply}")
